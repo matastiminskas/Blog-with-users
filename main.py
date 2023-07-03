beb019fa86ccae655d9899d8fa2cbc80e400727b
Matas <matas.timinskas@gmail.com>
2023-07-03 22:28:52 +0300

    add .gitignore and .evn files

--- main.py
+++ main.py
@@ -11,10 +11,14 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from flask_bcrypt import Bcrypt
 from functools import wraps
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("FLASK_SECRET_KEY")
 ckeditor = CKEditor(app)
 bootstrap = Bootstrap(app)
 bcrypt = Bcrypt(app)
